05fb6e003aa613fa57841bdb3d0bf7e4ca887ec2
mindoo <leon2@sautour.net>
2016-12-13 21:39:08 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Prototypes
+void gameMenu();
+
+/*
+Input : none
+Function : main entry point of the programm
+Output : integer (0 if execution went correctly or 1 if it didn't)
+*/
+int main() {
+
+  printf("Hello world \n");
+
+  return 0;
+}
